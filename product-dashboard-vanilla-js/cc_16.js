b4c51908395d908bec214dcf0050e4b5c5e22ed7
Kamali Hicks <kamalikh3@gmail.com>
2025-03-28 18:42:44 -0400

    Add fetchProductsAsync using async/await and try/catch

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -18,3 +18,19 @@ function fetchProductsThen() {
         console.error('An error occured while fetching products', error);
     })
 }
+
+// Task 3: Fetch Products with async/await
+
+async function fetchProductsAsync() {
+
+    try {const response = await fetch('https://www.course-api.com/javascript-store-products');
+        if (!response.ok) {
+            throw new Error('Network error');
+        }
+        const products  = await response.json(); 
+        displayProducts(products)
+        } catch (error) {
+            handleError(error);
+        }
+
+}
